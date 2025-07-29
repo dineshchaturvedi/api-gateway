676cea58f0ac3f06226b60ac2bfdd6c45c89944a
dineshchaturvedi <78488980+dineshchaturvedi@users.noreply.github.com>
2025-07-29 16:03:04 -0400

    working currency exchange and conversion service with API Gateway

--- src/main/java/com/dc/microservices/api_gateway/ApiGatewayConfirguration.java
+++ src/main/java/com/dc/microservices/api_gateway/ApiGatewayConfirguration.java
@@ -12,16 +12,25 @@ import org.springframework.context.annotation.Configuration;
 public class ApiGatewayConfirguration {
 
     @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-         return builder.routes()
+     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
                 .route(p -> p.path("/get")
                     .filters(f -> f
                         .addRequestHeader("MyHeader", "MyURI")
                         .addRequestParameter("Param", "MyValue"))
                     .uri("http://httpbin.org:80"))
-                .route(p1 -> p1.path("/currency-exchange/**")
-                    .uri("lb://currency-exchange"))
+
+                .route(p1 -> p1.path("/CURRENCY-EXCHANGE/**")
+                    .filters(f -> f.rewritePath("/CURRENCY-EXCHANGE/(?<remaining>.*)", "/${remaining}")) 
+                    .uri("lb://CURRENCY-EXCHANGE"))
+
+                .route(p2 -> p2.path("/CURRENCY-CONVERSION/**")
+                    .filters(f -> f.rewritePath("/CURRENCY-CONVERSION/(?<remaining>.*)", "/${remaining}"))
+                    .uri("lb://CURRENCY-CONVERSION"))
+                .route(p2 -> p2.path("/CURRENCY-CONVERSION-FEIGN/**")
+                    .filters(f -> f.rewritePath("/CURRENCY-CONVERSION-FEIGN/(?<remaining>.*)", "/${remaining}"))
+                    .uri("lb://CURRENCY-CONVERSION-FEIGN"))
+
                 .build();
     }
-
 }
